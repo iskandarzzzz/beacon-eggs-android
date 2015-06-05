32264c2ca3f7fec79d5cb2cf53274b06d3d11ab8
William Hollacsek <william.hollacsek@gmail.com>
2015-06-05 19:54:56 +0900

    Add userId

--- app/src/main/java/beaconeggs/android/App.java
+++ app/src/main/java/beaconeggs/android/App.java
@@ -2,8 +2,10 @@ package beaconeggs.android;
 
 import android.app.Application;
 import android.bluetooth.BluetoothAdapter;
+import android.content.SharedPreferences;
 
 import java.util.List;
+import java.util.UUID;
 
 import beaconeggs.android.editorModel.EditorLayout;
 import beaconeggs.android.service.BeaconHistory;
@@ -14,20 +16,35 @@ import beaconeggs.android.service.BeaconMonitorService;
  */
 public class App extends Application {
 
+    private static SharedPreferences pref;
     // Retrieve from server
     public List<EditorLayout> editorLayouts;
-
     // Set by respective Spinner
     public EditorLayout editorLayout;
     public BeaconHistory.FilterMethod filterMethod;
-
     // Set by NumberPicker
     public long foregroundScanPeriod = BeaconMonitorService.DEFAULT_FOREGROUND_SCAN_PERIOD;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        pref = getSharedPreferences("pref", MODE_PRIVATE);
+    }
 
     // Helper method
     static boolean hasBluetooth() {
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         return bluetoothAdapter != null;
+    }
+
+    public static String getUserId() {
+        String userId = pref.getString("userId", null);
+        if (userId == null) {
+            userId = UUID.randomUUID().toString();
+            pref.edit().putString("userId", userId);
+        }
+
+        return userId;
     }
 }
