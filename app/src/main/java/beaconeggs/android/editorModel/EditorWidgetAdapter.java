59c0d458d2d52f7ec7d095ac68c6f30e1c7346e4
William Hollacsek <william.hollacsek@gmail.com>
2015-06-05 19:58:22 +0900

    Handle exception when instantiating missing editor objects

--- app/src/main/java/beaconeggs/android/editorModel/EditorWidgetAdapter.java
+++ app/src/main/java/beaconeggs/android/editorModel/EditorWidgetAdapter.java
@@ -1,5 +1,7 @@
 package beaconeggs.android.editorModel;
 
+import android.util.Log;
+
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
@@ -11,6 +13,8 @@ import java.lang.reflect.Type;
  * Created by william on 18/10/14.
  */
 public class EditorWidgetAdapter implements JsonDeserializer<EditorWidget> {
+    private static final String TAG = "EditorWidgetAdapter";
+
     @Override
     public EditorWidget deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         String type = json.getAsJsonObject().get("type").getAsString();
@@ -19,7 +23,7 @@ public class EditorWidgetAdapter implements JsonDeserializer<EditorWidget> {
             Class<?> c = Class.forName("beaconeggs.android.editorModel.Editor" + type);
             return context.deserialize(json, c);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            Log.w(TAG, e.toString());
         }
 
         return null;
